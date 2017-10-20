e33444c84fba75d5f29074f15fa3a54c8e925d7d
Baylee Williams <bawilliams@redventures.net>
2017-10-19 23:31:06 -0400

    first commit

---
+++ C-Medium/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stars = 0;
+            for (var i = 0; i < 50; i++)
+            {
+                Console.Write("*");
+                stars++;
+                if (stars % 10 == 0)
+                {
+                    Console.Write("\n");
+                }
+
+            }
+
+        }
+    }
+}
+
